eed1b9628a50bea62c95515c46b6c7565cd5b4df
NBH <hoangphuong191058@gmail.com>
2023-11-22 23:01:21 +0700

    Update sftp_krx.sh

--- sftp_krx.sh
+++ sftp_krx.sh
@@ -6,8 +6,8 @@
 #	- Tested on Ubuntu 20
 #	- Script must run as root
 #
-#Adding directly to /etc/crontab file:
-# 30 18 * * 1-6 root sh ${script_path}/sftp_krx.sh | tee -a ${script_path}/logs/sftp_krx.log
+#Adding directly to /etc/crontab file (change the path if needed):
+# 30 18 * * 1-6 root sh /opt/apps/script/sftp_krx.sh | tee -a /opt/apps/script/logs/sftp_krx.log
 #Specific these values in SET PARAM block:
 #	hnx (ip1, ip2)
 #	hsx (ip1, ip2)
